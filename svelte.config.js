8e3bd2edde1c594b35d54c1aa15ea942b1fa0d16
artgp <git@arturoguzmanperez.com>
2025-03-06 11:22:13 +0000

    fix: svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,24 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+
+		// prerender: {
+		// 	handleHttpError: ({ path, referrer, message }) => {
+		// 		console.log(`first of message`)
+		// 		console.log(message)
+		// 		console.log(`last of message`)
+		// 		// ignore deliberate link to shiny 404 page
+		// 		if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+		// 			return
+		// 		}
+		// 	}
+		// }
 	},
 
 	extensions: ['.svelte', '.svx', '.md']
