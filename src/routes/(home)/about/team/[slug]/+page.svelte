ae38169b9991f68e82d7b00c8d9a709a2070e4c5
artgp <git@arturoguzmanperez.com>
2025-03-12 19:36:33 +0000

    feat: improve team member page

--- src/routes/(home)/about/team/[slug]/+page.svelte
+++ src/routes/(home)/about/team/[slug]/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import { goto } from '$app/navigation'
+	import { INSTITUTIONS, ROLES, SOCIAL_MEDIA } from '$lib/globals/data.js'
 	import { NAV_HEIGHT } from '$lib/globals/style'
+	import Content from '$lib/ui/blog/content.svelte'
 	import Title from '$lib/ui/blog/title.svelte'
 	import Anchor from '$lib/ui/buttons/anchor.svelte'
 	import IconButton from '$lib/ui/buttons/icon_button.svelte'
 	import BaseSection from '$lib/ui/sections/base_section.svelte'
-	import Fact from '$lib/ui/text/fact.svelte'
-	import Paragraph from '$lib/ui/text/paragraph.svelte'
-	import { jstr } from '@arturoguzman/art-ui'
+	import Subtitle from '$lib/ui/text/subtitle.svelte'
+	import { Picture } from '@arturoguzman/art-ui'
 	import {
+		IconBrandInstagram,
 		IconBrandLinkedin,
 		IconBrandMastodon,
 		IconBrandTwitter,
@@ -21,35 +22,49 @@
 <BaseSection>
 	<div class="team-member-section" style:--nav-height={NAV_HEIGHT}>
 		<div class="left-col">
-			<img class="profile-picture" src="https://placehold.co/600x1200/FF8F42/FF8F42" alt="" />
+			<div class="profile-picture">
+				<Picture image={data.team_member.picture}></Picture>
+			</div>
 			<div class="profile-metadata">
 				<Title size="large" title="{data.team_member.first_name} {data.team_member.last_name}"
 				></Title>
+				<div class="role">
+					<Subtitle subtitle={ROLES[data.team_member.role]}></Subtitle>
+					<Subtitle subtitle={INSTITUTIONS[data.team_member.institution]}></Subtitle>
+				</div>
 				<div class="social-media">
-					<IconButton
-						onclick={() => {
-							goto('https://www.linkedin.com/company/sdr-imago')
-						}}
-					>
-						<IconBrandLinkedin></IconBrandLinkedin>
-					</IconButton>
-					<IconButton>
-						<IconBrandMastodon></IconBrandMastodon>
-					</IconButton>
-					<IconButton>
-						<IconBrandTwitter></IconBrandTwitter>
-					</IconButton>
-					<IconButton>
-						<IconLink></IconLink>
-					</IconButton>
+					{#each data.team_member.url.filter((url) => SOCIAL_MEDIA.includes(url.title)) as url}
+						<IconButton
+							onclick={() => {
+								window.location = url.url
+							}}
+						>
+							{#if url.title.toLowerCase().includes('linkedin')}
+								<IconBrandLinkedin></IconBrandLinkedin>
+							{/if}
+							{#if url.title.toLowerCase().includes('mastodon')}
+								<IconBrandMastodon></IconBrandMastodon>
+							{/if}
+							{#if url.title.toLowerCase().includes('twitter')}
+								<IconBrandTwitter></IconBrandTwitter>
+							{/if}
+							{#if url.title.toLowerCase().includes('instagram')}
+								<IconBrandInstagram></IconBrandInstagram>
+							{/if}
+						</IconButton>
+					{/each}
 				</div>
 			</div>
 		</div>
 		<div class="right-col">
-			<Paragraph text={data.team_member.content}></Paragraph>
-			{#each data.team_member.url as url}
-				<Anchor label={url.title} href={url.url}></Anchor>
-			{/each}
+			<Content>
+				{@html data.team_member.content}
+			</Content>
+			<div class="external-links">
+				{#each data.team_member.url.filter((url) => !SOCIAL_MEDIA.includes(url.title)) as url}
+					<Anchor label={url.title} href={url.url}></Anchor>
+				{/each}
+			</div>
 		</div>
 	</div>
 </BaseSection>
@@ -57,24 +72,26 @@
 <style>
 	.team-member-section {
 		display: grid;
-		grid-template-columns: minmax(0, 3fr) minmax(0, 5fr);
+		grid-template-columns: minmax(0, 1fr);
 		color: var(--theme-colour-text);
 		gap: 3rem;
-		/* min-height: calc(100lvh - var(--nav-height)); */
 	}
 
 	.left-col {
 		width: 100%;
-		padding: 0 4rem;
 		overflow: hidden;
-		border-right: 1px solid var(--theme-colour-text);
 		display: grid;
+		grid-template-columns: minmax(0, 1fr);
+		grid-template-rows: minmax(0, 1fr) minmax(0, max-content);
 		gap: 2rem;
+		padding: 0 1rem;
+		border-bottom: 1px solid var(--theme-colour-text);
 	}
 	.profile-picture {
 		height: 100%;
 		object-fit: cover;
 		border-radius: 1rem;
+		overflow: hidden;
 	}
 	.profile-metadata {
 		display: flex;
@@ -93,5 +110,22 @@
 		color: white;
 		display: flex;
 		gap: 0.25rem;
+		margin-bottom: 2rem;
+	}
+	.external-links {
+		display: grid;
+		grid-template-columns: minmax(0, max-content);
+	}
+	@media (min-width: 1024px) {
+		.team-member-section {
+			grid-template-columns: minmax(0, 3fr) minmax(0, 5fr);
+			height: calc(100lvh - var(--nav-height) - 5rem);
+			margin-top: 1rem;
+		}
+		.left-col {
+			padding: 0 2rem;
+			border-bottom: initial;
+			border-right: 1px solid var(--theme-colour-text);
+		}
 	}
 </style>
