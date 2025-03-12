81060e8caef7afd62a78c684869f406325407267
artgp <git@arturoguzmanperez.com>
2025-03-12 19:29:59 +0000

    feat: add data globals

---
+++ src/lib/globals/data.ts
@@ -0,0 +1,13 @@
+export const ROLES: Record<'rse' | 'researcher' | 'postdoc' | 'administrator', string> = {
+	rse: 'Research Software Engineer',
+	researcher: 'Researcher',
+	postdoc: 'Postdoctoral Research Associate',
+	administrator: 'Administrator'
+}
+
+export const INSTITUTIONS: Record<'newcastle_university' | 'u_of_liverpool', string> = {
+	newcastle_university: 'Newcastle University',
+	u_of_liverpool: 'University of Liverpool'
+}
+
+export const SOCIAL_MEDIA: string[] = ['mastodon', 'linkedin', 'twitter', 'instagram']
