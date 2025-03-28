329329d4007497da9ec7b976a74107ef1f55de63
artgp <git@arturoguzmanperez.com>
2025-03-28 10:56:29 +0000

    feat: add rss feed

--- src/lib/globals/data.ts
+++ src/lib/globals/data.ts
@@ -15,3 +15,7 @@ export const INSTITUTIONS: Record<'newcastle_university' | 'u_of_liverpool', str
 }
 
 export const SOCIAL_MEDIA: string[] = ['mastodon', 'linkedin', 'twitter', 'instagram']
+
+// NOTE: switch this when there's an active runtime for the website
+// export const FEEDS = ['rss', 'feed', 'atom', 'rss.xml']
+export const FEEDS = ['rss']
---
+++ src/routes/[feeds=feeds]/careers.xml/+server.ts
@@ -0,0 +1,56 @@
+import { FEEDS } from '$lib/globals/data.js'
+import { readItems } from '@directus/sdk'
+import { Feed } from 'feed'
+import { DateTime } from 'luxon'
+const title = 'Imago - Smart Data Research UK | Careers'
+const url = 'https://imago.ac.uk/careers'
+export const prerender = true
+export const entries = async () => {
+	return FEEDS.map((entry) => ({ feeds: entry }))
+}
+export const GET = async ({ locals }) => {
+	const feed = new Feed({
+		title: title,
+		description:
+			'Imago unlocks the potential of satellite imagery to provide a much richer understanding of urgent challenges facing the UK.',
+		id: url,
+		link: url,
+		language: 'en',
+		favicon: `${url}favicon.png`,
+		copyright: `Copyright ${new Date().getFullYear().toString()}, ${title}`,
+		generator: '',
+		feedLinks: {
+			rss: `${url}rss.xml`
+		},
+		author: {
+			name: 'Imago team',
+			email: '',
+			link: url
+		},
+		ttl: 60
+	})
+	const careers = await locals.directus.request(
+		readItems('careers', { fields: ['slug', 'title', 'content', 'date_created', 'date_updated'] })
+	)
+	careers.forEach((post) => {
+		feed.addItem({
+			title: post.title,
+			description: String(post.content),
+			content: String(post.content),
+			link: `${url}careers/${post.slug}`,
+			author: [
+				{
+					name: 'Imago team',
+					email: '',
+					link: url
+				}
+			],
+			date: DateTime.fromISO(String(post.date_created)).toJSDate()
+		})
+	})
+	return new Response(feed.rss2(), {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8'
+		}
+	})
+}
