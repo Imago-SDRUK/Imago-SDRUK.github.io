1c492c2df3eea81cddbf00dec08e2f80b3ebe7c2
artgp <git@arturoguzmanperez.com>
2025-04-29 09:20:39 +0100

    fix: sort events and careers based on end date

--- src/routes/(home)/careers/+page.server.ts
+++ src/routes/(home)/careers/+page.server.ts
@@ -1,7 +1,7 @@
 import { readItems } from '@directus/sdk'
 
 export async function load({ locals }) {
-	const careers = await locals.directus.request(readItems('careers'))
+	const careers = await locals.directus.request(readItems('careers', { sort: '-closing_date' }))
 	return {
 		careers
 	}
--- src/routes/(home)/events/+page.server.ts
+++ src/routes/(home)/events/+page.server.ts
@@ -1,7 +1,7 @@
 import { readItems } from '@directus/sdk'
 
 export async function load({ locals }) {
-	const events = await locals.directus.request(readItems('events'))
+	const events = await locals.directus.request(readItems('events', { sort: '-date_end' }))
 	return {
 		events
 	}
