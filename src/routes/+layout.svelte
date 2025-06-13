916c4487f19305906729311ececc6bdde5e88d7d
artgp <git@arturoguzmanperez.com>
2025-06-13 12:08:53 +0100

    refactor: global theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,26 +6,9 @@
 	import '@fontsource-variable/figtree'
 	import '@fontsource-variable/space-grotesk'
 	import { PUBLIC_NODE_ENV } from '$env/static/public'
-
 	import { window_height, window_width } from '$lib/stores/layout.svelte'
+	import { theme } from '$lib/ui/utils/theme'
 	let { children } = $props()
-	const theme = {
-		colours: {
-			background: '#24226f',
-			text: '#e2e5f3',
-			primary: '#ff8f42',
-			secondary: '#4584a5',
-			tertiary: '#8c91a8',
-			quarternary: '#4a4a49',
-			highlight: '#03cea3'
-		},
-		fonts: {
-			title: 'Figtree Variable',
-			subtitle: 'Quicksand Variable',
-			paragraph: 'Noto Sans Variable',
-			accent: 'Space Grotesk Variable'
-		}
-	}
 </script>
 
 <svelte:head>
