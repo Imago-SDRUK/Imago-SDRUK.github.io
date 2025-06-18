767dc2e7964d20cef19c37e34b9dc81421199a4d
artgp <git@arturoguzmanperez.com>
2025-06-18 09:24:01 +0100

    feat: mobile menu as tabs

--- src/lib/ui/nav/nav.svelte
+++ src/lib/ui/nav/nav.svelte
@@ -10,7 +10,8 @@
 	import { window_height, window_width } from '$lib/stores/layout.svelte'
 	let scroll = $state(0)
 	let desktop = $derived($window_width > 768)
-	let menu_open = $state(false)
+	let menu_open = $state(true)
+	let mobile_menu_selection = $state(ROUTES[0].href ?? '')
 	const getBgPercentage = (scroll: number, height: number) => {
 		const result = (scroll * 100) / height
 		if (result > 100) return 100
@@ -67,20 +68,57 @@
 			</button>
 		{/if}
 		<div class="routes" data-menu={menu_open ? true : undefined}>
-			{#each ROUTES as { href, label, subpaths }}
-				<Menu>
-					{#snippet trigger({ toggleMenu })}
-						<NavButton onpointerdown={() => toggleMenu(false)} {href} {label}></NavButton>
-					{/snippet}
-					{#snippet children()}
-						<div class="subpaths">
+			{#if desktop}
+				{#each ROUTES as { href, label, subpaths }}
+					<Menu>
+						{#snippet trigger({ toggleMenu })}
+							{#if desktop}
+								<NavButton onpointerdown={() => toggleMenu(false)} {href} {label}></NavButton>
+							{/if}
+						{/snippet}
+						{#snippet children()}
+							<div class="subpaths">
+								{#each subpaths as { href, label }}
+									<Anchor {href} {label}></Anchor>
+								{/each}
+							</div>
+						{/snippet}
+					</Menu>
+				{/each}
+			{:else}
+				<div class="mobile-menu">
+					<div class="mobile-menu-tabs">
+						{#each ROUTES as { href, label }}
+							{#if href === mobile_menu_selection}
+								<a
+									onclick={() => {
+										menu_open = false
+									}}
+									{href}><p>{label}</p></a
+								>
+							{:else}
+								<button
+									onclick={() => {
+										mobile_menu_selection = href
+									}}><p>{label}</p></button
+								>
+							{/if}
+						{/each}
+					</div>
+					<div class="mobile-menu-routes">
+						{#each ROUTES.filter((route) => route.href === mobile_menu_selection) as { subpaths }}
 							{#each subpaths as { href, label }}
-								<Anchor {href} {label}></Anchor>
+								<a
+									onclick={() => {
+										menu_open = false
+									}}
+									{href}><p>{label}</p></a
+								>
 							{/each}
-						</div>
-					{/snippet}
-				</Menu>
-			{/each}
+						{/each}
+					</div>
+				</div>
+			{/if}
 		</div>
 	</div>
 </nav>
@@ -135,21 +173,41 @@
 		gap: 1rem;
 		justify-content: flex-end;
 		align-items: center;
-		transform: translate(200%, 75%);
+		transform: translate(200%, 4rem);
 		border: 1px solid var(--theme-colour-highlight);
 		border-radius: 0.35rem;
 		background-color: var(--theme-colour-background);
 		transition: all 0.3s ease-in-out;
 		padding: 2rem;
+		width: calc(100% - 2rem);
 	}
 	.routes[data-menu] {
 		z-index: 2;
-		transform: translate(0%, 75%);
+		transform: translate(0%, 4rem);
+		/* width: calc(100% - 2rem); */
 	}
 	.icon {
 		height: 2rem;
 	}
 	.subpaths {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	.mobile-menu {
+		display: grid;
+		grid-template-columns: minmax(0, 1fr);
+		grid-template-rows: minmax(0, max-content) minmax(0, 1fr);
+		width: 100%;
+		gap: 1rem;
+	}
+	.mobile-menu-tabs {
+		display: flex;
+		justify-content: space-around;
+		border-bottom: 1px solid var(--theme-colour-secondary);
+	}
+	.mobile-menu-routes {
 		display: flex;
 		flex-direction: column;
 		gap: 0.5rem;
@@ -172,6 +230,7 @@
 			gap: 1rem;
 			justify-content: flex-end;
 			align-items: center;
+			width: initial;
 		}
 	}
 </style>
