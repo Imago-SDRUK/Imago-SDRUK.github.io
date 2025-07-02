976f68a2323165f742ee4d3ef6ea44569da741a5
artgp <git@arturoguzmanperez.com>
2025-07-02 10:20:03 +0100

    feat: image endpoint accepts query params

--- src/routes/assets/[...id]/+server.ts
+++ src/routes/assets/[...id]/+server.ts
@@ -1,17 +1,12 @@
 import { assetEndpoint } from '@arturoguzman/art-ui'
-import { directusSDKWithToken, handleDirectusError } from '$lib/utils/directus.js'
-import { readItems } from '@directus/sdk'
 import { env } from '$env/dynamic/private'
-// export const prerender = true
-// export const entries = async () => {
-// 	const entries = await directusSDKWithToken(env.BACKEND_TOKEN)
-// 		.request(readItems('team_members'))
-// 		.catch(handleDirectusError)
-// 	return entries
-// 		.filter((event) => event.slug !== null)
-// 		.map((event) => ({ id: String(event.picture) }))
-// }
 
-export const GET = async ({ fetch, params }) => {
-	return assetEndpoint(params.id, `${env.BACKEND_URL}/assets`, fetch, undefined, env.BACKEND_TOKEN)
+export const GET = async ({ fetch, url, params }) => {
+	return assetEndpoint(
+		params.id,
+		`${env.BACKEND_URL}/assets`,
+		fetch,
+		encodeURI(url.searchParams.toString()),
+		env.BACKEND_TOKEN
+	)
 }
